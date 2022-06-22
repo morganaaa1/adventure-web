1af2a93583e1ac681fd8ab19cd2bb715212e1524
Reza Ageng <rezaageng5@gmail.com>
2022-06-22 13:36:40 +0700

    style(navbar): :lipstick: add transparent navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,28 @@
-import React from "react";
-import styled from "styled-components";
+import styled from "styled-components"
 
 const Container = styled.div`
-    height: 60px;
+  width: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  z-index: 50;
 `
-
-const Wrapper = styled.div`
-    display: flex ;
-    align-items: center ;
-    justify-content: space-between ;
-`
-
-const Left = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: center;
-`;
-
-
-const Center = styled.div`
-    flex: 1;
-    text-align: center;
-`;
 
 const Logo = styled.h1`
-    color:white ;
-    font-size: 32px ;
-    font-weight: bold;
-`;
-const Right = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-`;
+  color: white;
+  font-size: 32px;
+  font-weight: bold;
+`
 
 const Navbar = () => {
-    return(
-        <Container>
-            <Wrapper>
-                <Center>
-                    <Logo>ADVENTURE.</Logo>
-                </Center>
-            </Wrapper>
-        </Container>
-    )
+  return (
+    <Container>
+      <Logo>ADVENTURE.</Logo>
+    </Container>
+  )
 }
 
-export default Navbar;
+export default Navbar
